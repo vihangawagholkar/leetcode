399728120f81da4b30f03b43bfe5d83f7e8dee17
Vihanga Wagholkar <vihanga.wagholkar@gmail.com>
2026-03-07 20:53:07 -0500

    Add Two sum with Hash Map and Brute Force

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,37 +1,41 @@
-// #include <iostream>
-// #include <vector>
-
-// using namespace std;
-
-// int main(){
-//         vector <int> nums;
-//         int i;
-//         while(cin>>i){
-//             nums.push_back(i);
-//         }
-//         int target;
-//         cin>> target;
-//         vector <int> result;
-//         for(int i=0; i<nums.size(); i++){
-//             for(int k=i+1; k<nums.size(); k++){
-//                 if(target == nums[i] + nums[k]){
-//                     result.push_back(i);
-//                     result.push_back(k);}
-//             }
-//         }
-
-//         for(int i=0; i<result.size(); i++){
-//             cout<<result[i]<<" ";
-//         }a
-    
-//     return 0;
-    
-// }
-
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n; // number of elements in the array
+//     vector<int> nums(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> nums[i];
+//     }
+
+//     int target;
+//     cin >> target;
+
+//     vector<int> result;
+//     for (int i = 0; i < nums.size(); i++) {
+//         for (int k = i + 1; k < nums.size(); k++) {
+//             if (nums[i] + nums[k] == target) {
+//                 result.push_back(i);
+//                 result.push_back(k);
+//                 break; // stop inner loop after finding a valid pair
+//             }
+//         }
+//         if (!result.empty()) break; // stop outer loop after finding a valid pair
+//     }
+
+//     for (int i = 0; i < result.size(); i++) {
+//         cout << result[i] << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// };
+
+// Hash map solution for better time complexity
 
 int main() {
     int n;
@@ -44,21 +48,22 @@ int main() {
     int target;
     cin >> target;
 
-    vector<int> result;
-    for (int i = 0; i < nums.size(); i++) {
-        for (int k = i + 1; k < nums.size(); k++) {
-            if (nums[i] + nums[k] == target) {
-                result.push_back(i);
-                result.push_back(k);
-                break; // stop inner loop after finding a valid pair
-            }
-        }
-        if (!result.empty()) break; // stop outer loop after finding a valid pair
-    }
+    unordered_map<int, int> numMap; // map to store number and its index
+    
+    // for(int i=0; i<nums.size(); i++){
+    //     numMap[nums[i]] = i;
+    // }
+    
+    for(int i=0; i<nums.size(); i++){
+        int complement = target - nums[i];
+        if(numMap.find(complement) != numMap.end() && numMap[complement] != i){
+            cout<< numMap[complement] <<" "<< i;
+            break;}
 
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i] << " ";
+        numMap[nums[i]] = i;
     }
+    
+    
     cout << endl;
 
     return 0;
